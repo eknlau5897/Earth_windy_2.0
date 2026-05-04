681c408c56dcc3515cc37cb00ad32ffd4a3de2ab
eknlau@gmail.com <eknlau@gmail.com>
2026-05-04 10:02:43 +0800

    update

--- demo/demo.js
+++ demo/demo.js
@@ -6,7 +6,7 @@ function initDemoMap() {
     }
   );
 
-   L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+   var ESri_DarkGreyCanvas=L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
             maxZoom: 20
         });

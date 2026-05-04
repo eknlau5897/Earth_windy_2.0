13c0acc5ecff69e461948bd0ae7f480557d06c97
eknlau@gmail.com <eknlau@gmail.com>
2026-05-04 09:59:16 +0800

    update

--- demo/demo.js
+++ demo/demo.js
@@ -1,15 +1,15 @@
 function initDemoMap() {
   var Esri_WorldImagery = L.tileLayer(
-    "http://arcgisonline.com{z}/{y}/{x}",
+    "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
     {
       attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
     }
   );
 
-  var Esri_DarkGreyCanvas = L.tileLayer('https://{s}://{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors &copy; <a href="https://carto.com">CARTO</a>',
-    maxZoom: 20
-  });
+   L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            maxZoom: 20
+        });
 
   var StreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'

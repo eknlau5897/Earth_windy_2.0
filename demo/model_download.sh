854a70d0a14f50d2965b10b15196417f306a5a5a
eknlau5897 <eknlau@gmail.com>
2026-06-10 14:37:15 +0800

    update

--- demo/model_download.sh
+++ demo/model_download.sh
@@ -100,7 +100,7 @@ while true; do
         CLEAN_HOUR=$((10#$fhr))
         export CLEAN_HOUR; export fhr
 
-        python3 - <<EOF
+        python3.11 - <<EOF
 import os, sys, json, gzip
 import numpy as np
 from herbie import Herbie
